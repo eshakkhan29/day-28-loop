615b1890deb1b76bb430ef202cae28cb11901b6e
eshakkhan29 <eshakkhan29@gmail.com>
2022-01-29 10:18:38 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,24 @@
+// loop
+
+//  while lopp 
+
+var num = 0;
+while (num <= 10) {
+    console.log("amare bar bar dekhao", num);
+    num++;
+}
+
+// for loop
+
+for (var i = 0; i < 15; i++) {
+    console.log(i);
+    
+}
+
+// lop in array 
+
+var array = ["eshak", "zybayer", "masud", "mahmud", "naim vai"]
+for (var i = 0; i < array.length; i++) {
+    var element = array[i];
+    console.log(element);
+}
