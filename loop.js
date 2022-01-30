c68869ca88e77cb8d731161815255690e372b8a7
eshakkhan29 <eshakkhan29@gmail.com>
2022-01-30 22:58:15 +0600

    lol

--- loop.js
+++ loop.js
@@ -22,3 +22,24 @@ for (var i = 0; i < array.length; i++) {
     var element = array[i];
     console.log(element);
 }
+
+// loop diye bar bar jor bejor sokha ber kora 
+
+// jor sonkha ber kora 
+let from = 5;
+let to = 10;
+for (let i = from; i <= to ; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// biyog namber 
+let theke = 2;
+let jabe = 20;
+for (let i = theke; i < jabe; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+    
+}
